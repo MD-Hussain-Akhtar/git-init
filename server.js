3c604fa9c83ad4863ee3de49b56f432dc89bb54c
MD-Hussain-Akhtar <mhussainakhter@gmail.com>
2026-03-22 17:43:32 -0400

    Update chat app: avatars, timestamps, WhatsApp style

--- server.js
+++ server.js
@@ -3,21 +3,24 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
-app.use(express.static('public'));
+const PORT = 3000;
+
+// Serve static files from "public"
+app.use(express.static(__dirname + '/public'));
 
 io.on('connection', (socket) => {
     console.log('A user connected');
 
-    // Receive username from client
-    socket.on('set username', (username) => {
-        socket.username = username; // store username for this socket
-        console.log(`${username} joined the chat`);
+    // Store username and avatar
+    socket.on('set username', (data) => {
+        socket.username = data.username;
+        socket.avatar = data.avatar;
+        console.log(`${socket.username} joined the chat`);
     });
 
-    // Receive chat message from client
-    socket.on('chat message', (msg) => {
-        // Include username with message
-        io.emit('chat message', { user: socket.username, message: msg });
+    // Broadcast chat messages
+    socket.on('chat message', (data) => {
+        io.emit('chat message', data);
     });
 
     socket.on('disconnect', () => {
@@ -25,4 +28,6 @@ io.on('connection', (socket) => {
     });
 });
 
-http.listen(3000, () => console.log('Server running on http://localhost:3000'));
+http.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
