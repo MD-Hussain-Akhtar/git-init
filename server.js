ecadf3602739da9d0b7c4889220921c233c1f91d
MD-Hussain-Akhtar <mhussainakhter@gmail.com>
2026-03-24 14:35:40 -0400

    Updated chat UI with WhatsApp-style bubbles

--- server.js
+++ server.js
@@ -5,26 +5,32 @@ const io = require('socket.io')(http);
 
 const PORT = 3000;
 
-// Serve static files from "public"
 app.use(express.static(__dirname + '/public'));
 
 io.on('connection', (socket) => {
     console.log('A user connected');
 
-    // Store username and avatar
     socket.on('set username', (data) => {
         socket.username = data.username;
-        socket.avatar = data.avatar;
-        console.log(`${socket.username} joined the chat`);
     });
 
-    // Broadcast chat messages
+    // Text message
     socket.on('chat message', (data) => {
         io.emit('chat message', data);
     });
 
+    // Voice message
+    socket.on('voice message', (data) => {
+        io.emit('voice message', data);
+    });
+
+    // Image message
+    socket.on('image message', (data) => {
+        io.emit('image message', data);
+    });
+
     socket.on('disconnect', () => {
-        console.log(`${socket.username || 'A user'} disconnected`);
+        console.log('User disconnected');
     });
 });
 
