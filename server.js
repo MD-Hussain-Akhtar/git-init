12a157aac8cafde48510483c212df4d770d46645
MD-Hussain-Akhtar <mhussainakhter@gmail.com>
2026-03-24 17:29:11 -0400

    Updated chat app: removed keydown comments and other improvements

--- server.js
+++ server.js
@@ -5,31 +5,34 @@ const io = require('socket.io')(http);
 
 const PORT = 4000;
 
-// Serve static files from public folder
 app.use(express.static(__dirname + '/public'));
 
 io.on('connection', (socket) => {
-    console.log('A user connected');
+  console.log('A user connected');
 
-    socket.on('set username', (data) => {
-        socket.username = data.username;
-    });
+  socket.on('set username', data => { socket.username = data.username; });
 
-    // Text message
-    socket.on('chat message', (data) => io.emit('chat message', data));
+  // Text messages
+  socket.on('chat message', data => io.emit('chat message', data));
 
-    // Voice message
-    socket.on('voice message', (data) => io.emit('voice message', data));
+  // Voice messages (recorded)
+  socket.on('voice message', data => io.emit('voice message', data));
 
-    // Image message
-    socket.on('image message', (data) => io.emit('image message', data));
+  // Image messages
+  socket.on('image message', data => io.emit('image message', data));
 
-    // Video call signaling
-    socket.on('video-offer', data => socket.broadcast.emit('video-offer', data));
-    socket.on('video-answer', data => socket.broadcast.emit('video-answer', data));
-    socket.on('ice-candidate', data => socket.broadcast.emit('ice-candidate', data));
+  // Video call signaling
+  socket.on('video-offer', data => socket.broadcast.emit('video-offer', data));
+  socket.on('video-answer', data => socket.broadcast.emit('video-answer', data));
 
-    socket.on('disconnect', () => console.log('User disconnected'));
+  // Voice call signaling
+  socket.on('voice-offer', data => socket.broadcast.emit('voice-offer', data));
+  socket.on('voice-answer', data => socket.broadcast.emit('voice-answer', data));
+
+  // ICE candidates
+  socket.on('ice-candidate', data => socket.broadcast.emit('ice-candidate', data));
+
+  socket.on('disconnect', () => console.log('User disconnected'));
 });
 
 http.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
